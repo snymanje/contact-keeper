72f00cd53af7a18593de3bd24d240dc01ac78f41
jeansn <jeansn@tfg.co.za>
2020-10-24 13:10:15 +0200

    backend done

--- config/db.js
+++ config/db.js
@@ -3,20 +3,25 @@ const config = require("config");
 const e = require("express");
 const db = config.get("mongoURI");
 
-const connectDB = () => {
-  mongoose
-    .connect(db, {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-      useUnifiedTopology: true,
-    })
-    .then(() => {
-      console.log("Connected to DB");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+const connectDB = async () => {
+  try {
+    await mongoose
+      .connect(db, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+      })
+      .then(() => {
+        console.log("Connected to DB");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  } catch (err) {
+    console.log(err.message);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const connectDB = require("./config/db");
-
+const userRoutes = require("./routes/user");
+const authRoutes = require("./routes/auth");
+const contactRoutes = require("./routes/contacts");
 const app = express();
 
 connectDB();
+
+app.use(express.json({ extended: true }));
+
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/contacts", contactRoutes);
 
 const PORT = process.env.PORT || 5000;
 
